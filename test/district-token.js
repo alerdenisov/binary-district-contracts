7f2e3be885f589768421b6d12e3339a5c36c1a00
Aler Denisov <aler.zampillo@gmail.com>
2017-10-31 02:13:19 +0300

    test token

---
+++ test/district-token.js
@@ -0,0 +1,27 @@
+const DistrictToken = artifacts.require('./DistrictToken.sol')
+const BigNumber = require('bignumber.js')
+const { expectThrow } = require('./utils.js')
+
+function toToken (number) {
+  return number * Math.pow(10, 18)
+}
+
+contract('DistrictToken', accounts => {
+  let token
+
+  beforeEach(async() => {
+    token = await DistrictToken.deployed()
+  })
+
+  describe('Hardcap test', async() => {
+    it('Should have a hard cap', async() => {
+      const hardCap = await token.HARDCAP()
+      assert(hardCap.toString(10) === "1000000000000000000000000000")
+    })
+
+    it('Shouldnt mint more than hard cap', async() => {
+      await token.mint(accounts[0], await token.HARDCAP(), { from: accounts[0] })
+      expectThrow(token.mint(accounts[0], 1, { from: accounts[0] }))
+    })
+  })
+})
